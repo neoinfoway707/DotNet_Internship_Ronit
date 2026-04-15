3d63d664d038c087b39b8d69b07c61b4908e310b
KinjalP <kinjal@truesys.co.in>
2026-04-15 18:15:38 +0530

    Implementation C# task of Day-03 to Day-07

---
+++ Internship tasks/Projects/DIDemo/DIDemo/D4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIDemo
+{
+    internal class D4
+    {
+        public static void Run()
+        {
+            Console.WriteLine("#2499 D4");
+            Car c = new Car("BMW", "X5", "2020");
+            c.Multiple();
+        }
+       
+    }
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public string Year;
+
+        public Car(string Brand, string Model, string Year)
+        {
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Year = Year;
+        }
+
+        public void Multiple()
+        {
+            Console.WriteLine($"Brand: {Brand}, Model: {Model}, Year: {Year}");
+        }
+    }
+}
--- Internship tasks/Projects/DIDemo/DIDemo/Program.cs
+++ Internship tasks/Projects/DIDemo/DIDemo/Program.cs
@@ -20,7 +20,12 @@ class Program
 
         
         //D1.Run();
-        D2.Run();
+        //D2.Run();
+        //D3.Run();
+        //D4.Run();
+        //D5.Run();
+        //D6.Run();
+        D7.Run();
 
 
 
